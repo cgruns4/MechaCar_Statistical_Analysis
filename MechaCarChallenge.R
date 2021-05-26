54acaf2d9de8b48eb988b5cbc3ecca68c970120e
cgruns4 <71041680+cgruns4@users.noreply.github.com>
2021-05-25 22:38:34 -0400

    deliverable 3, t -tests

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,12 +1,13 @@
+#DELIVERABLE 1
 library(dplyr)
-library(tidyverse)
+
 Mecha_Car_df <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) #import Mecha_Car dataset
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Mecha_Car_df) #generate multiple linear regression model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Mecha_Car_df)) #generate summary statistics
 
-
+#DELIVERABLE 2
 Suspension_Coil <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F) #import Suspension_Coil dataset
 
 Coil_df <-data.frame(Suspension_Coil)
@@ -17,3 +18,13 @@ summary(total_summary)
 
 lot_summary <- Coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
 summary(lot_summary)
+
+
+#DELIVERABLE 3
+t.test(Coil_df$PSI,mu = 1500)
+
+t.test(subset(Coil_df,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+
+t.test(subset(Coil_df,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+
+t.test(subset(Coil_df,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
